309521946fcb9d29f72adab6b9a5ad6a12cb8e49
Esteban Abarca <eabarca171@gmail.com>
2021-11-16 17:28:35 -0300

    firebaseConfig

---
+++ src/Firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAsrc3BPR9eo9EFImf7blGAfKOxZe1GJ3s",
+    authDomain: "osbirec.firebaseapp.com",
+    projectId: "osbirec",
+    storageBucket: "osbirec.appspot.com",
+    messagingSenderId: "1008240252230",
+    appId: "1:1008240252230:web:9074b44c5002b6a8940147"
+  };
+
+  export default firebaseConfig
